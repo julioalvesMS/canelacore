08edacb09b3db136ea53b61623e3b0824a878f68
julioalvesMS <julioalves.mesquita@gmail.com>
2016-02-03 02:19:08 -0200

    UpdateInventory agora mostra a unidade e suporta atualizações do estoque e entrada de produtos

--- inventory.py
+++ inventory.py
@@ -46,7 +46,7 @@ class InventoryManager(wx.Frame):
         button_categories.Bind(wx.EVT_BUTTON, self.open_category_manager)
         panel_buttons_left = wx.Panel(panel_top, pos=(140, 40), size=(500, 40), style=wx.SIMPLE_BORDER)
         see = GenBitmapTextButton(panel_buttons_left, -1,
-                                  wx.Bitmap(core.directory_paths['icons'] + 'Tools.png', wx.BITMAP_TYPE_PNG),
+                                  wx.Bitmap(core.directory_paths['icons'] + 'Search.png', wx.BITMAP_TYPE_PNG),
                                   u'Ver Mais', pos=(0, 0), size=(100, 40))
         see.SetBackgroundColour(core.COLOR_DEFAULT_BACKGROUND)
         see.Bind(wx.EVT_BUTTON, self.data_open)
@@ -406,9 +406,13 @@ class UpdateInventory(wx.Frame):
     textbox_product_id = None
     textbox_product_price = None
     textbox_product_amount = None
+    textbox_product_unit = None
 
     list_inventory = None
     list_update = None
+
+    radio_update = None
+    radio_entry = None
 
     panel_product_data = None
     __panel_product = None
@@ -432,12 +436,27 @@ class UpdateInventory(wx.Frame):
         self.SetIcon((wx.Icon(core.ICON_MAIN, wx.BITMAP_TYPE_ICO)))
         self.Centre()
 
-        self.list_update = wx.ListCtrl(self, -1, pos=(10, 280), size=(450, 295),
+        self.list_update = wx.ListCtrl(self, -1, pos=(10, 280), size=(450, 275),
                                        style=wx.LC_REPORT | wx.SIMPLE_BORDER | wx.LC_VRULES | wx.LC_HRULES)
         self.list_update.InsertColumn(0, u"ID", width=50)
         self.list_update.InsertColumn(1, u"Descrição", width=180)
         self.list_update.InsertColumn(2, u"Quantidade")
         self.list_update.InsertColumn(3, u"Preço Unit.")
+
+        self.radio_update = wx.RadioButton(self, -1, label=u'Atualizar estoque', pos=(10, 560), size=(225, 25),
+                                           style=wx.SIMPLE_BORDER)
+        self.radio_entry = wx.RadioButton(self, -1, label=u'Entrada de produtos', pos=(235, 560), size=(225, 25),
+                                          style=wx.SIMPLE_BORDER)
+
+        self.radio_update.SetFont(wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.radio_entry.SetFont(wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD))
+
+        self.radio_update.SetToolTip(wx.ToolTip(u'Dar uma nova quantidade em estoque para os produtos.'
+                                                u'\nIgnora um estoque anterior.'))
+        self.radio_entry.SetToolTip(wx.ToolTip(u'Atualiza o estoque de acordo com uma entrada de produtos.'
+                                               u'\nApenas registra uma variação no estoque.'))
+
+        self.radio_entry.SetValue(True)
 
         # product
         self.panel_product_data = wx.Panel(self, 22, pos=(10, 10), size=(450, 260),
@@ -455,8 +474,8 @@ class UpdateInventory(wx.Frame):
         self.textbox_product_description.SetDescriptiveText(u'Descrição do produto')
         self.list_inventory = wx.ListCtrl(self.panel_product_data, -1, pos=(10, 45), size=(430, 115),
                                           style=wx.LC_REPORT | wx.LC_VRULES | wx.LC_HRULES | wx.SIMPLE_BORDER)
-        self.list_inventory.InsertColumn(0, u'ID')
-        self.list_inventory.InsertColumn(1, u'Descrição', width=230)
+        self.list_inventory.InsertColumn(0, u'Descrição', width=230)
+        self.list_inventory.InsertColumn(1, u'Estoque')
         self.list_inventory.InsertColumn(2, u'Preço')
         self.list_inventory.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.database_select)
 
@@ -473,6 +492,10 @@ class UpdateInventory(wx.Frame):
         self.textbox_product_amount = wx.SearchCtrl(self.panel_product_data, -1, pos=(230, 170), size=(100, -1))
         self.textbox_product_amount.ShowSearchButton(False)
         self.textbox_product_amount.SetDescriptiveText(u'Quantidade')
+
+        self.textbox_product_unit = wx.TextCtrl(self.panel_product_data, -1, pos=(335, 175), size=(50, -1),
+                                                style=wx.NO_BORDER | wx.TE_READONLY)
+        self.textbox_product_unit.SetBackgroundColour(core.COLOR_DEFAULT_BACKGROUND)
 
         self.__panel_product = wx.Panel(self.panel_product_data, size=(300, 40), pos=(100, 210),
                                         style=wx.SIMPLE_BORDER)
@@ -520,9 +543,7 @@ class UpdateInventory(wx.Frame):
 
     def clean(self):
         self.list_update.DeleteAllItems()
-        self.textbox_product_amount.Clear()
-        self.textbox_product_price.Clear()
-        self.textbox_product_description.Clear()
+        self.data_editor_disable(None)
         self.database_search(None)
 
     def open_product_register(self, event):
@@ -545,14 +566,18 @@ class UpdateInventory(wx.Frame):
             self.textbox_product_amount.Clear()
             return dialogs.launch_error(self, u'Quantidade inválida!')
 
+        unit = self.textbox_product_unit.GetValue()
+
         unit_price = core.format_cash_user(product.price, currency=True)
-        item = self.list_update.Append((core.format_id_user(product.ID), product.description, core.format_amount_user(amount), unit_price))
+        item = self.list_update.Append((core.format_id_user(product.ID), product.description,
+                                        core.format_amount_user(amount, unit=unit), unit_price))
         self.list_update.SetItemData(item, product_id)
 
         self.textbox_product_amount.Clear()
         self.textbox_product_id.Clear()
         self.textbox_product_description.Clear()
         self.textbox_product_price.SetValue(u"0,00")
+        self.textbox_product_unit.Clear()
 
     def data_delete(self, event):
         if self.list_update.GetFocusedItem() == -1:
@@ -562,12 +587,14 @@ class UpdateInventory(wx.Frame):
     def data_editor_enable(self, event):
         self.item = self.list_update.GetFocusedItem()
         if not self.item == -1:
+            amount, unit = self.list_update.GetItemText(self.item, 2).split()
             self.textbox_product_description.SetValue(self.list_update.GetItemText(self.item, 1))
             self.textbox_product_id.SetValue(str(self.list_update.GetItemData(self.item)))
             self.textbox_product_price.SetValue(self.list_update.GetItemText(self.item, 3))
-            self.textbox_product_amount.SetValue(self.list_update.GetItemText(self.item, 2))
+            self.textbox_product_amount.SetValue(amount)
+            self.textbox_product_unit.SetValue(unit)
             self.__panel_product.Destroy()
-            self.__panel_product = wx.Panel(self.panel_product_data, size=(200, 40), pos=(200, 225),
+            self.__panel_product = wx.Panel(self.panel_product_data, size=(200, 40), pos=(200, 210),
                                             style=wx.SIMPLE_BORDER)
             eplus = GenBitmapTextButton(self.__panel_product, 220,
                                         wx.Bitmap(core.directory_paths['icons'] + 'Edit.png', wx.BITMAP_TYPE_PNG),
@@ -596,10 +623,11 @@ class UpdateInventory(wx.Frame):
             return dialogs.launch_error(self, u'Quantidade inválida!')
 
         unit_price = core.format_cash_user(product.price, currency=True)
+        unit = self.textbox_product_unit.GetValue()
 
         self.list_update.SetStringItem(self.item, 0, core.format_id_user(product.ID))
         self.list_update.SetStringItem(self.item, 1, product.description)
-        self.list_update.SetStringItem(self.item, 2, amount)
+        self.list_update.SetStringItem(self.item, 2, core.format_amount_user(amount, unit=unit))
         self.list_update.SetStringItem(self.item, 3, unit_price)
 
         self.list_update.SetItemData(self.item, product_id)
@@ -607,12 +635,13 @@ class UpdateInventory(wx.Frame):
         self.data_editor_disable(event)
 
     def data_editor_disable(self, event):
+        self.textbox_product_unit.Clear()
         self.textbox_product_amount.Clear()
-        self.textbox_product_price.SetValue(u'0,00')
+        self.textbox_product_price.Clear()
         self.textbox_product_id.Clear()
         self.textbox_product_description.Clear()
         self.__panel_product.Destroy()
-        self.__panel_product = wx.Panel(self.panel_product_data, size=(300, 40), pos=(100, 225), style=wx.SIMPLE_BORDER)
+        self.__panel_product = wx.Panel(self.panel_product_data, size=(300, 40), pos=(100, 210), style=wx.SIMPLE_BORDER)
         button_add_product = GenBitmapTextButton(self.__panel_product, 220,
                                                  wx.Bitmap(core.directory_paths['icons'] + 'Add.png',
                                                            wx.BITMAP_TYPE_PNG),
@@ -637,14 +666,19 @@ class UpdateInventory(wx.Frame):
         self.list_inventory.DeleteAllItems()
         product_list = self.database_inventory.inventory_search_description(self.textbox_product_description.GetValue())
         for product in product_list:
-            self.list_inventory.Append((product.ID, product.description,
-                                        core.format_cash_user(product.price, currency=True)))
+            category = self.database_inventory.categories_search_id(product.category_ID)
+            item = self.list_inventory.Append((product.description,
+                                               core.format_amount_user(product.amount, category.unit),
+                                               core.format_cash_user(product.price, currency=True)))
+            self.list_inventory.SetItemData(item, product.ID)
 
     def database_select(self, event):
         j = self.list_inventory.GetFocusedItem()
+        unit = self.list_inventory.GetItemText(j, 1).split()[-1]
         self.textbox_product_price.SetValue(self.list_inventory.GetItemText(j, 2))
-        self.textbox_product_id.SetValue(self.list_inventory.GetItemText(j, 0))
-        self.textbox_product_description.SetValue(self.list_inventory.GetItemText(j, 1))
+        self.textbox_product_id.SetValue(str(self.list_inventory.GetItemData(j)))
+        self.textbox_product_description.SetValue(self.list_inventory.GetItemText(j, 0))
+        self.textbox_product_unit.SetValue(unit)
         self.textbox_product_amount.SetFocus()
 
     def end(self):
@@ -653,11 +687,16 @@ class UpdateInventory(wx.Frame):
         if w == 0:
             return dialogs.launch_error(self, u'Você não adicionou nenhum produto!')
 
+        update = self.radio_update.GetValue()
+        entry = self.radio_entry.GetValue()
         db = database.InventoryDB()
         for i in range(w):
             products_id = self.list_update.GetItemData(i)
             amount = core.amount2float(self.list_update.GetItemText(i, 2))
-            db.update_product_amount(products_id, amount)
+            if update:
+                db.update_product_amount(products_id, amount)
+            elif entry:
+                db.update_product_stock(products_id, amount)
 
         db.close()
 
