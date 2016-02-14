d2e0eb1966b75d33c531755b5d96916b001ce4b4
julioalvesMS <julioalves.mesquita@gmail.com>
2016-02-14 16:31:39 -0200

    Novo Frame para gerenciar Contas e Ganhos

--- transaction.py
+++ transaction.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import threading
 
 import wx
 import wx.gizmos
@@ -19,6 +20,223 @@ INCOME = 1
 EXPENSE = 2
 
 
+class TransactionManager(wx.Frame):
+
+    notebook_lists = None
+
+    list_expenses = None
+    list_incomes = None
+
+    combobox_month_displayed = None
+
+    def __init__(self, parent, title=u'Transaçoes'):
+        wx.Frame.__init__(self, parent, -1, title,
+                          style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX | wx.CLIP_CHILDREN)
+
+        self.setup_gui()
+
+        db = database.TransactionsDB()
+        dates = db.list_transactions_dates()
+        db.close()
+
+        months = list()
+
+        for date in dates:
+            month = core.format_date_user(date[:-3])
+            if month not in months:
+                months.append(month)
+        if months:
+            self.combobox_month_displayed.SetItems(months)
+            self.combobox_month_displayed.SetSelection(0)
+
+        self.setup(None)
+        self.Show()
+
+    def setup_gui(self):
+        self.SetPosition(wx.Point(100, 100))
+        self.SetSize(wx.Size(960, 560))
+        self.SetIcon((wx.Icon(core.ICON_MAIN, wx.BITMAP_TYPE_ICO)))
+        self.SetBackgroundColour(core.COLOR_DEFAULT_BACKGROUND)
+        panel_top = wx.Panel(self, pos=(10, 10), size=(1180, 100))
+
+        button_categories = GenBitmapTextButton(panel_top, -1, wx.Bitmap(core.directory_paths['icons'] + 'Drawer.png',
+                                                wx.BITMAP_TYPE_PNG), u'Categorias', pos=(5, 40), size=(115, 40),
+                                                style=wx.SIMPLE_BORDER)
+        button_categories.SetBackgroundColour(core.COLOR_DEFAULT_BACKGROUND)
+        button_categories.Bind(wx.EVT_BUTTON, self.open_category_manager)
+
+        panel_buttons_left = wx.Panel(panel_top, pos=(140, 40), size=(400, 40), style=wx.SIMPLE_BORDER)
+
+        see = GenBitmapTextButton(panel_buttons_left, -1,
+                                  wx.Bitmap(core.directory_paths['icons'] + 'Search.png', wx.BITMAP_TYPE_PNG),
+                                  u'Ver Mais', pos=(0, 0), size=(100, 40))
+        see.SetBackgroundColour(core.COLOR_DEFAULT_BACKGROUND)
+        see.Bind(wx.EVT_BUTTON, self.data_open)
+        plus = GenBitmapTextButton(panel_buttons_left, -1,
+                                   wx.Bitmap(core.directory_paths['icons'] + 'contact-new.png', wx.BITMAP_TYPE_PNG),
+                                   u'Novo', pos=(100, 0), size=(100, 40))
+        plus.SetBackgroundColour(core.COLOR_DEFAULT_BACKGROUND)
+        plus.Bind(wx.EVT_BUTTON, self.open_new_transaction)
+
+        edi = GenBitmapTextButton(panel_buttons_left, -1,
+                                  wx.Bitmap(core.directory_paths['icons'] + 'Edit.png', wx.BITMAP_TYPE_PNG), u'Editar',
+                                  pos=(200, 0), size=(100, 40))
+        edi.SetBackgroundColour(core.COLOR_DEFAULT_BACKGROUND)
+        edi.Bind(wx.EVT_BUTTON, self.data_edit)
+
+        era = GenBitmapTextButton(panel_buttons_left, -1,
+                                  wx.Bitmap(core.directory_paths['icons'] + 'Trash.png', wx.BITMAP_TYPE_PNG),
+                                  u'Apagar', pos=(300, 0), size=(100, 40))
+        era.SetBackgroundColour(core.COLOR_DEFAULT_BACKGROUND)
+        era.Bind(wx.EVT_BUTTON, self.ask_delete)
+
+        self.combobox_month_displayed = wx.ComboBox(panel_top, -1, pos=(565, 45), size=(100, 30), style=wx.CB_READONLY)
+
+        self.combobox_month_displayed.Bind(wx.EVT_TEXT_ENTER, self.setup)
+
+        panel_buttons_right = wx.Panel(panel_top, pos=(690, 40), size=(240, 40), style=wx.SIMPLE_BORDER)
+        quir = GenBitmapTextButton(panel_buttons_right, -1, wx.Bitmap(core.directory_paths['icons'] + 'Exit.png'),
+                                   u'Sair',
+                                   pos=(120, 0), size=(120, 40))
+        quir.SetBackgroundColour(core.COLOR_DEFAULT_BACKGROUND)
+        quir.Bind(wx.EVT_BUTTON, self.exit)
+        rep = GenBitmapTextButton(panel_buttons_right, -1, wx.Bitmap(core.directory_paths['icons'] + 'Reset.png'),
+                                  u'Atualizar',
+                                  pos=(0, 0), size=(120, 40))
+        rep.SetBackgroundColour(core.COLOR_DEFAULT_BACKGROUND)
+        rep.Bind(wx.EVT_BUTTON, self.setup)
+        self.notebook_lists = wx.Notebook(self, -1, pos=(15, 110), size=(930, 410))
+        self.notebook_lists.SetBackgroundColour(core.COLOR_DEFAULT_BACKGROUND)
+        for i in range(2):
+            list_transactions = wx.ListCtrl(self.notebook_lists, -1,
+                                            style=wx.LC_VRULES | wx.LC_HRULES | wx.SIMPLE_BORDER | wx.LC_REPORT)
+            list_transactions.InsertColumn(0, u'Descrição', width=300)
+            list_transactions.InsertColumn(1, u'ID', width=80)
+            list_transactions.InsertColumn(2, u'Categoria', width=200)
+            list_transactions.InsertColumn(3, u'Valor', width=110)
+            list_transactions.InsertColumn(4, u'Data', width=110)
+            list_transactions.InsertColumn(5, u'Pagamento', width=120)
+
+            list_transactions.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.data_open)
+
+            if not i:
+                self.list_expenses = list_transactions
+                text = u'Contas e Gastos'
+            else:
+                self.list_incomes = list_transactions
+                text = u'Ganhos e Entradas'
+
+            self.notebook_lists.AddPage(list_transactions, text)
+
+    def setup(self, event):     # FIXME Fazer a thread fechar direito com o resto do app
+        rest = threading.Thread(target=self.__setup__)
+        rest.start()
+
+    def __setup__(self):
+        self.setup_monthly_expenses()
+        self.setup_monthly_incomes()
+
+    def setup_monthly_incomes(self):
+        self.list_incomes.DeleteAllItems()
+        month = core.format_date_internal(self.combobox_month_displayed.GetValue())
+
+        db = database.TransactionsDB()
+        incomes = db.monthly_transactions_list(month, INCOME)
+        for income in incomes:
+            category = db.categories_search_id(income.category)
+            self.list_incomes.Append((income.description, core.format_id_user(income.ID),
+                                      category.category if category else u'',
+                                      core.format_cash_user(income.value, currency=True),
+                                      core.format_date_user(income.transaction_date),
+                                      u'OK' if not income.payment_pendant else u'Pendente'))
+        db.close()
+
+    def setup_monthly_expenses(self):
+        self.list_expenses.DeleteAllItems()
+        month = core.format_date_internal(self.combobox_month_displayed.GetValue())
+
+        db = database.TransactionsDB()
+        expenses = db.monthly_transactions_list(month, EXPENSE)
+        for expense in expenses:
+            category = db.categories_search_id(expense.category)
+            self.list_expenses.Append((expense.description, core.format_id_user(expense.ID),
+                                       category.category if category else u'',
+                                       core.format_cash_user(expense.value, currency=True),
+                                       core.format_date_user(expense.transaction_date),
+                                       u'OK' if not expense.payment_pendant else u'Pendente'))
+        db.close()
+
+    def data_delete(self, event):
+
+        if self.notebook_lists.GetSelection():
+            data_list = self.list_incomes
+
+        else:
+            data_list = self.list_expenses
+
+        it = data_list.GetFocusedItem()
+        if it == -1:
+            return
+        e_id = data_list.GetItem(it, 1).GetText()
+
+        db = database.TransactionsDB()
+        db.delete_transaction(int(e_id))
+        db.close()
+        self.setup(None)
+
+    def data_edit(self, event):
+        if self.notebook_lists.GetSelection():
+            data_list = self.list_incomes
+            transaction_type = INCOME
+        else:
+            data_list = self.list_expenses
+            transaction_type = EXPENSE
+
+        it = data_list.GetFocusedItem()
+        if it == -1:
+            return
+
+        key = data_list.GetItem(it, 1).GetText()
+
+        Transaction(self, transaction_type=transaction_type, key=key)
+
+    def data_open(self, event):
+        if self.notebook_lists.GetSelection():
+            data_list = self.list_incomes
+            transaction_type = INCOME
+        else:
+            data_list = self.list_expenses
+            transaction_type = EXPENSE
+
+        it = data_list.GetFocusedItem()
+        if it == -1:
+            return
+
+        key = data_list.GetItem(it, 1).GetText()
+
+        Transaction(self, transaction_type=transaction_type, key=key, editable=False)
+
+    def ask_delete(self, event):
+        if self.notebook_lists.GetSelection():
+            dialogs.Ask(self, u'Apagar Transação', 26)
+        else:
+            dialogs.Ask(self, u'Apagar Transação', 22)
+
+    def open_category_manager(self, event):
+        categories.TransactionCategoryManager(self)
+
+    def open_new_transaction(self, event):
+        if self.notebook_lists.GetSelection():
+            transaction_type = INCOME
+        else:
+            transaction_type = EXPENSE
+
+        Transaction(self, transaction_type=transaction_type)
+
+    def exit(self, event):
+        self.Close()
+
+
 class Transaction(wx.Frame):
 
     textbox_description = None
@@ -32,7 +250,7 @@ class Transaction(wx.Frame):
 
     categories_ids = [None]
 
-    def __init__(self, parent, transaction_type, key=-1, data=None):
+    def __init__(self, parent, transaction_type, key=-1, data=None, editable=True):
 
         title_options = {
             EXPENSE: u'Despesa',
@@ -46,6 +264,7 @@ class Transaction(wx.Frame):
         self.key = key
         self.data = data
         self.transaction_type = transaction_type
+        self.editable = editable
 
         self.setup_gui()
 
@@ -96,19 +315,41 @@ class Transaction(wx.Frame):
         # last
         last = wx.Panel(self, -1, size=(130, 250), pos=(515, 10), style=wx.SIMPLE_BORDER)
         last.SetBackgroundColour(core.COLOR_DEFAULT_BACKGROUND)
-        last_ = wx.Panel(last, pos=(5, 65), size=(120, 120), style=wx.SIMPLE_BORDER)
-        finish = GenBitmapTextButton(last_, -1,
-                                     wx.Bitmap(core.directory_paths['icons'] + 'Check.png', wx.BITMAP_TYPE_PNG),
-                                     u'Finalizar', pos=(0, 0), size=(120, 40))
-        finish.Bind(wx.EVT_BUTTON, self.ask_end)
-        restart = GenBitmapTextButton(last_, -1,
-                                      wx.Bitmap(core.directory_paths['icons'] + 'Reset.png', wx.BITMAP_TYPE_PNG),
-                                      u'Recomeçar', pos=(0, 40), size=(120, 40))
-        restart.Bind(wx.EVT_BUTTON, self.ask_clean)
-        cancel = GenBitmapTextButton(last_, -1,
-                                     wx.Bitmap(core.directory_paths['icons'] + 'Exit.png', wx.BITMAP_TYPE_PNG),
-                                     u"sair", pos=(0, 80), size=(120, 40))
-        cancel.Bind(wx.EVT_BUTTON, self.ask_exit)
+
+        if self.editable:
+
+            last_ = wx.Panel(last, pos=(5, 65), size=(120, 120), style=wx.SIMPLE_BORDER)
+            finish = GenBitmapTextButton(last_, -1,
+                                         wx.Bitmap(core.directory_paths['icons'] + 'Check.png', wx.BITMAP_TYPE_PNG),
+                                         u'Finalizar', pos=(0, 0), size=(120, 40))
+            finish.Bind(wx.EVT_BUTTON, self.ask_end)
+            restart = GenBitmapTextButton(last_, -1,
+                                          wx.Bitmap(core.directory_paths['icons'] + 'Reset.png', wx.BITMAP_TYPE_PNG),
+                                          u'Recomeçar', pos=(0, 40), size=(120, 40))
+            restart.Bind(wx.EVT_BUTTON, self.ask_clean)
+            cancel = GenBitmapTextButton(last_, -1,
+                                         wx.Bitmap(core.directory_paths['icons'] + 'Exit.png', wx.BITMAP_TYPE_PNG),
+                                         u"sair", pos=(0, 80), size=(120, 40))
+            cancel.Bind(wx.EVT_BUTTON, self.ask_exit)
+
+        else:
+            self.textbox_description.Disable()
+            self.textbox_value.Disable()
+            self.combobox_category.Disable()
+            self.calendar_date.Disable()
+            self.checkbox_payed.Disable()
+
+            last_ = wx.Panel(last, pos=(5, 85), size=(120, 80), style=wx.SIMPLE_BORDER)
+            edit = GenBitmapTextButton(last_, 243,
+                                       wx.Bitmap(core.directory_paths['icons'] + 'Edit.png', wx.BITMAP_TYPE_PNG),
+                                       u"Editar",
+                                       pos=(0, 0), size=(120, 40))
+            edit.Bind(wx.EVT_BUTTON, self.open_transaction_edit)
+            cancel = GenBitmapTextButton(last_, 242,
+                                         wx.Bitmap(core.directory_paths['icons'] + 'Exit.png', wx.BITMAP_TYPE_PNG),
+                                         u"Sair",
+                                         pos=(0, 40), size=(120, 40))
+            cancel.Bind(wx.EVT_BUTTON, self.exit)
 
     def setup(self):
         if self.data:
@@ -121,6 +362,7 @@ class Transaction(wx.Frame):
             return
         self.textbox_description.SetValue(self.data.description)
         self.textbox_value.SetValue(core.format_cash_user(self.data.value, currency=True))
+        self.checkbox_payed.SetValue(not self.data.payment_pendant)
         date = self.data.transaction_date.split('-')
         wx_date = wx.DateTime()
         wx_date.Set(int(date[2]), int(date[1]) - 1, int(date[0]))
@@ -159,6 +401,10 @@ class Transaction(wx.Frame):
 
     def open_category_register(self, event):
         categories.TransactionCategoryData(self)
+
+    def open_transaction_edit(self, event):
+        Transaction(parent=self.GetParent(), transaction_type=self.transaction_type, key=self.key, data=self.data)
+        self.exit(None)
 
     def update_categories(self):
         db = database.TransactionsDB()
